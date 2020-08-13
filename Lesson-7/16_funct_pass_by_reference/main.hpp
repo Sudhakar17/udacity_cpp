3d060da2a85037f32c58eb9a8df57f98c90c7197
Sudhakar17 <sudhakarece17@gmail.com>
2020-08-13 15:27:17 +0200

    solution for funct pass by reference

---
+++ Lesson-7/16_funct_pass_by_reference/main.hpp
@@ -0,0 +1,34 @@
+/*The header file for main.cpp*/
+
+#include <iostream>
+
+void calculate(float input1, float input2, char op, float &result);
+void printEquation(float input1, float input2, char operation, float result);
+
+void calculate(float input1, float input2, char op, float &result)
+{
+    switch (op)
+    {
+        case '+':
+            result = input1 + input2;
+            break;
+        case '-':
+            result = input1 - input2;
+            break;
+        case '*':
+            result = input1 * input2;
+            break;
+        case '/':
+            result = input1 / input2;
+            break;
+        default:  
+            std::cout<<"Illegal operation\n";
+    }
+    
+}
+
+void printEquation(float input1, float input2, char operation, float result)
+{
+    std::cout << input1 << operation << input2 << "=" << result;   
+    
+}
