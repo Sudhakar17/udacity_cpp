6dafec042c0291eb3faca203d0b43e4376cd140b
Sudhakar17 <sudhakarece17@gmail.com>
2020-08-25 16:53:53 +0200

    header file for class Student-Array

---
+++ Lesson-8/21_classes_and_arrays/main.hpp
@@ -0,0 +1,70 @@
+// header file for main.cpp
+
+#include<iostream>
+
+class Student
+{
+    int grade[5];
+    int id;
+    public:
+        Student();
+        void setId(int idIn);
+        void setGrade(int index, int gradeIn);
+        int getId();
+        int getGrade(int index);
+        float getAvg();
+        void printInfo();
+};
+
+Student::Student()
+{
+    id = 0;
+    for (int i=0; i<5; i++)
+    {
+        grade[i] = 0;
+    }
+}
+
+
+void Student::setId(int idIn)
+{
+    id = idIn;
+}
+
+void Student::setGrade(int index, int gradeIn)
+{
+    grade[index] = gradeIn;
+}
+
+
+int Student::getId()
+{
+    return id;
+}
+
+int Student::getGrade(int index)
+{
+    return grade[index];
+}
+
+float Student::getAvg()
+{   
+    int sum = 0;
+    float avg = 0;
+    for (int i=0; i<5; i++)
+    {
+        sum += grade[i];
+    }
+    avg = sum / 5.0;
+    return avg;
+}
+
+void Student::printInfo()
+{
+    std::cout<<id<<"\n";
+    for (int i=0; i<5; i++)
+    {
+        std::cout<<grade[i]<< " ";
+    }
+
+}
