e806905ef2d4292f449d362a3de4eb4ef28b1f68
Sudhakar17 <sudhakarece17@gmail.com>
2020-08-25 14:39:07 +0200

    main file for helper function

---
+++ Lesson-8/18_helper_functions_in_classes/main.cpp
@@ -0,0 +1,17 @@
+// Goal: Look at helper functions in classes
+
+#include "main.hpp"
+
+int main()
+{
+    Student s1;
+    s1.setId(3232);
+    s1.setGrade(0, 85);
+    s1.setGrade(1, 80);
+    s1.setGrade(2, 98);
+    s1.setGrade(3, 65);
+    s1.setGrade(4, 90);
+    s1.printInfo();
+    std::cout << "avg = " << s1.getAvg();
+    return 0;
+}
