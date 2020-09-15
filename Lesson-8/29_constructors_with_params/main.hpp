66f8b0ed49588eea4da7ca57ab0fa9e9ae9a5fa8
Sudhakar17 <sudhakarece17@gmail.com>
2020-09-15 10:55:07 +0200

    header file for constructor with params

---
+++ Lesson-8/29_constructors_with_params/main.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+class Patient
+{
+    private:
+        std::string name;
+    public:
+        // The constructor accepts a name parameter
+        Patient(std::string input);
+        void setName(std::string input);
+        std::string getName();
+};
+
+
+Patient::Patient(std::string input)
+{
+    // when an object is created, the name must be added as a param
+    name = input;
+}
+
+void Patient::setName(std::string input)
+{
+    name = input;
+}
+
+std::string Patient::getName()
+{
+    return name;
+}
