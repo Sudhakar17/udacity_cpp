3a8ffe75bc8e99fb6b08e714811ce21e0fee1282
Sudhakar17 <sudhakarece17@gmail.com>
2020-09-24 13:56:28 +0200

    virtual function example

---
+++ Lesson-14/5_interview_questions.cpp
@@ -0,0 +1,54 @@
+// virtual function example
+
+#include<iostream>
+
+using namespace std;
+class A
+{
+     public: 
+           //TODO: run the code with and without
+           //the keyword 'virtual'
+        //   void virtual getMe();
+        void  getMe();
+           A();
+};
+void A::getMe()
+{
+    cout<<"A!";
+}
+A::A()
+{
+    cout<<" A constructor";
+}
+class B: public A
+{
+    public:
+           void getMe();
+           B();
+};
+B::B()
+{
+    cout<<" B constructor";
+}
+void B::getMe()
+{
+    cout<<"B!";
+}
+
+int main()
+{
+    cout<<"\nCreating instances:";
+    A *instanceB;
+    A *instanceA;
+    cout<<"\nCreating 'new' instances:";
+    cout<<"\n\tinstanceB: ";
+    instanceB = new B();
+    cout<<"\n\tinstanceA: ";
+    instanceA = new A();
+    
+    cout<<"\ninstanceB->getMe() produces: ";
+    instanceB->getMe();
+    cout<<"\ninstanceA->getMe() produces: ";
+    instanceA -> getMe();
+    return 0;
+}
