bf11014f8d643a5deee9da65cf6b31fb7240d8e7
Sudhakar17 <sudhakarece17@gmail.com>
2020-09-24 13:54:36 +0200

    guess the value of i and j

---
+++ Lesson-14/2_interview_questions.cpp
@@ -0,0 +1,13 @@
+/*Put your code here
+What will be the value of i and j after the following code is executed?*/
+#include<iostream>
+
+int main()
+{
+    //Put your code here
+    int i = 10;
+    int j = i++;
+    std::cout<<i<<" "<<j;
+    
+    return 0;
+}
